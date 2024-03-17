29bdeb1dfd0d9658e48ac2518ddf899d9d9d5851
annabenari <annabenari@me.com>
2024-03-17 14:55:29 +0000

    first day of 100 learning python

---
+++ main.py
@@ -0,0 +1,12 @@
+#Day 1
+
+
+print("hello")
+print("I am learning Python")
+# This prints to the Console
+
+print("""This Prints
+      To The Console 
+            Exactly how I 
+                typed  it on the screen""")
+# Using triple "" will print the text to the console exactly how I i typed it e.g. spacing and different line
